23dfd682d7778ae04a5273564d36626d318971b5
Thomas Nichols <ta.nichols@outlook.com>
2017-12-10 08:48:07 -0600

    Added CORS support for API calls

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 // require api routes into the application
 require('./server/routes')(app)
 
