f63f413b32e7cf32d55f6cdbe3ff47f673332859
Thomas Nichols <ta.nichols@outlook.com>
2017-12-10 13:53:00 -0600

    Added authentication to admin routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,20 @@
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+
 const categoriesController = require('../controllers').categories
 const coinsController = require('../controllers').coins
+
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestPerMinute: 5,
+    jwksUri: 'https://tanichols.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'coinvault',
+  issuer: 'https://tanichols.auth0.com/',
+  algorithms: ['RS256']
+})
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -7,16 +22,16 @@ module.exports = (app) => {
   }))
 
   app.get('/api/categories', categoriesController.list) // all
-  app.post('/api/categories', categoriesController.create) // admin
+  app.post('/api/categories', authCheck, categoriesController.create) // admin
   app.get('/api/categories/:categoryId', categoriesController.retreive) // all
-  app.put('/api/categories/:categoryId', categoriesController.update) // admin
-  app.delete('/api/categories/:categoryId', categoriesController.destroy) // admin
+  app.put('/api/categories/:categoryId', authCheck, categoriesController.update) // admin
+  app.delete('/api/categories/:categoryId', authCheck, categoriesController.destroy) // admin
 
   app.get('/api/categories/:categoryId/coins', coinsController.list) // all
-  app.post('/api/categories/:categoryId/coins', coinsController.create) // admin
+  app.post('/api/categories/:categoryId/coins', authCheck, coinsController.create) // admin
   app.get('/api/categories/:categoryId/coins/:coinId', coinsController.retreive) // all
-  app.put('/api/categories/:categoryId/coins/:coinId', coinsController.update) // admin
-  app.delete('/api/categories/:categoryId/coins/:coinId', coinsController.destroy) // admin
+  app.put('/api/categories/:categoryId/coins/:coinId', authCheck, coinsController.update) // admin
+  app.delete('/api/categories/:categoryId/coins/:coinId', authCheck, coinsController.destroy) // admin
 
   app.get('/api/coins', coinsController.list) // all
 }
