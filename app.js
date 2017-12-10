4252d3309bd1c470d60db10eb3b06083f5eca84a
Thomas Nichols <ta.nichols@outlook.com>
2017-12-10 17:57:23 -0600

    Added cors module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const logger = require('morgan')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 // Set up the express app
@@ -11,12 +12,7 @@ app.use(logger('dev'))
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+app.options('*', cors())
 
 // require api routes into the application
 require('./server/routes')(app)
